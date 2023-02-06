aae34759ced904808a9a2f003564827b091d3368
Alessandro Montenero <alemonte99@icloud.com>
2023-02-06 16:36:46 +0100

    created array of objects

---
+++ script/script.js
@@ -0,0 +1,34 @@
+let teamMembers = [
+    member1 = {
+        'name': 'Wayne Barnett',
+        'role': 'Founder & CEO',
+        'imgSearch': 'wayne-barnett-founder-ceo.jpg'
+    },
+    member2 = {
+        'name': 'Angela Caroll',
+        'role': 'Chief Editor',
+        'imgSearch': 'angela-caroll-chief-editor.jpg'
+    },
+    member3 = {
+        'name': 'Walter Gordon',
+        'role': 'Office Manager',
+        'imgSearch': 'walter-gordon-office-manager.jpg'
+    },
+    member4 = {
+        'name': 'Angela Lopez',
+        'role': 'Social Media Manager',
+        'imgSearch': 'angela-lopez-social-media-manager.jpg'
+    },
+    member5 = {
+        'name': 'Scott Estrada',
+        'role': 'Developer',
+        'imgSearch': 'scott-estrada-developer.jpg'
+    },
+    member6 = {
+        'name': 'Barbara Ramos',
+        'role': 'Graphic Designer',
+        'imgSearch': 'barbara-ramos-graphic-designer.jpg'
+    },
+]
+
+console.log(teamMembers)
